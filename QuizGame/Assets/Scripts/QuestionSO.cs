e1842a5dcd4724419c288f1b8416553d9c069748
Dylan Tivnan <tivnand@gmail.com>
2023-02-09 12:26:18 -0500

    Added GetQuestion method

--- QuizGame/Assets/Scripts/QuestionSO.cs
+++ QuizGame/Assets/Scripts/QuestionSO.cs
@@ -10,4 +10,8 @@ public class QuestionSO : ScriptableObject
     [SerializeField] string question = "Enter new question text here";
 
 
+    public string GetQuestion()
+    {
+        return question;
+    }
 }
